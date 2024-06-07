34c24fcbb3b55718376eafda12b5ddeafad156b3
Mostafa Ibrahim <moustafaebrahim124@gmail.com>
2024-06-07 12:19:37 +0300

    "create components"

---
+++ src/app/components/nav-blank/nav-blank.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-blank',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-blank.component.html',
+  styleUrl: './nav-blank.component.scss'
+})
+export class NavBlankComponent {
+
+}
